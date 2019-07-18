480e0617317343b260f88bca6e214f01cb3d5a86
Luc Tielen <luc.tielen@gmail.com>
2019-07-18 17:21:53 +0200

    Add first version of IR1->IR2 pass

---
+++ lib/Besra/Types/IR1.hs
@@ -0,0 +1,29 @@
+
+module Besra.Types.IR1
+  ( module Besra.Types.IR1.Module
+  , module Besra.Types.IR1.Expr
+  , module Besra.Types.IR1.TypeAnn
+  , module Besra.Types.IR1.Scheme
+  , module Besra.Types.IR1.Pred
+  , module Besra.Types.IR1.Type
+  , module Besra.Types.IR1.Tycon
+  , module Besra.Types.IR1.Tyvar
+  , module Besra.Types.IR1.Lit
+  , module Besra.Types.IR1.String
+  , module Besra.Types.IR1.Number
+  , module Besra.Types.IR1.Pattern
+  ) where
+
+import Besra.Types.IR1.Module
+import Besra.Types.IR1.Expr
+import Besra.Types.IR1.TypeAnn
+import Besra.Types.IR1.Scheme
+import Besra.Types.IR1.Pred
+import Besra.Types.IR1.Type
+import Besra.Types.IR1.Tycon
+import Besra.Types.IR1.Tyvar
+import Besra.Types.IR1.Lit
+import Besra.Types.IR1.String
+import Besra.Types.IR1.Number
+import Besra.Types.IR1.Pattern
+
