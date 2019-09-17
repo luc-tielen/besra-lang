76bf276ff8e224ab022a12acb01735fde830c4c2
Luc Tielen <luc.tielen@gmail.com>
2019-09-17 14:58:18 +0200

    Fix build with GHC8.8.1 and Cabal 3.0.0.0

--- default.nix
+++ default.nix
@@ -3,21 +3,29 @@
 with pkgs;
 
 let
+  inherit (haskell.lib) doJailbreak dontCheck;
   haskellPackages = haskell.packages.${compiler};
   hpack = haskellPackages.callPackage ./nix/hpack.nix {};
   haskellPkgs = haskellPackages.override {
     overrides = self: super: {
       hpack = self.callCabal2nix "hpack" hpack {};
+      prettyprinter = self.prettyprinter_1_3_0;
+      protolude = doJailbreak super.protolude;
+      neat-interpolation = dontCheck super.neat-interpolation;
     };
   };
+  # We have to call hpack now ourselves since current hpack is broken
+  hpack2cabal = name: src: pkgs.runCommand "hpack2cabal-${name}" {} ''
+    ${hpack}/bin/hpack '${src}' - > "$out"
+  '';
   source = nix-gitignore.gitignoreSource [] ./.;
-  #drv = haskellPkgs.callCabal2nix "besra" source {};
+  processedSource = hpack2cabal "besra" source;
+  drv = haskellPkgs.callCabal2nix "besra" processedSource {};
 in
   {
-    #besra = drv;
+    besra = drv;
     besra-shell = haskellPkgs.shellFor {
-      #packages = p: [ drv ];
-      packages = p: [];
+      packages = p: [ drv ];
       buildInputs = with haskellPkgs; [
         cabal-install
         hpack
