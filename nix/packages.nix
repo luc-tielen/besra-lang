b33ec0179e18d9c81e1c4b9f56bace4558833908
Luc Tielen <luc.tielen@gmail.com>
2019-09-18 08:54:17 +0200

    Point to newer nixpkgs

--- nix/packages.nix
+++ nix/packages.nix
@@ -1,9 +1,9 @@
 let
-  commit = "6f120bbe8427e374f17dcc361be231c9da85f51b";
+  commit = "4c6e9a553872366e485766fa8b574b54b15957f8";
   nixpkgs = builtins.fetchTarball {
     name = "nixpkgs-19.09";
-    url = "https://github.com/luc-tielen/nixpkgs/archive/${commit}.tar.gz";
-    sha256 = "19c38dk9g0a80pbnkd96lw9d68k3dmakib7gwby5ik1wd31vm2i2";
+    url = "https://github.com/nixos/nixpkgs/archive/${commit}.tar.gz";
+    sha256 = "1az9yg2lwwfw3aypi6bdsk7ghld2963vqdp8ajkxciyxdfkaxb3b";
   };
   pkgs = import nixpkgs;
 in
